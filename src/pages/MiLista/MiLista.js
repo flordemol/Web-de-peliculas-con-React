14f5b26cc50f4c790176b98139cb45d5bfc20ae8
Maria Florencia De Mollein <florenciademollein@gmail.com>
2021-02-12 11:00:59 -0300

    Agregué Head en page Mi Lista

--- src/pages/MiLista/MiLista.js
+++ src/pages/MiLista/MiLista.js
@@ -1,10 +1,12 @@
 import { Container, Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import Item from './Item';
+import Head from './../../Components/Common/Head';
 
 const MiLista = ({dispatch, lista : {lista}}) => {
     return ( 
         <>
+            <Head title="Mi Lista" description="¡Guardá tus películas favoritas!" />
             <Container fluid className="justify-content-center  align-items-center p-0" style={{marginTop:"120px", minHeight:"55vh"}}>
                 <h2 className="text-white" style={{margin: "50px"}}>Mi Lista</h2>
                 <Row className="m-0">  
